997d68b0c96ac3eb04253a39db41585f86774e57
jimutGitHub <JimutGitHub@users.noreply.github.com>
2019-02-12 15:33:20 +0530

    Nested Component implementation.
    Employee Component implementation as a nested component of AppComponent.

--- Angular2Demo/src/app/Employee/employee.component.js
+++ Angular2Demo/src/app/Employee/employee.component.js
@@ -7,17 +7,20 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 var core_1 = require("@angular/core");
-var AppComponent = /** @class */ (function () {
-    function AppComponent() {
-        this.pageHeader = 'Employee Details';
+var EmployeeComponent = /** @class */ (function () {
+    function EmployeeComponent() {
+        this.firstName = "Raghu";
+        this.lastName = "Rawat";
+        this.gender = "Male";
+        this.age = 27;
     }
-    AppComponent = __decorate([
+    EmployeeComponent = __decorate([
         core_1.Component({
-            selector: 'my-app',
-            template: "\n                <div>\n                    <h1>{{pageHeader}}</h1>\n                    <my-employee></my-employee>\n                </div>\n              "
+            selector: 'my-employee',
+            templateUrl: 'app/Employee/employee.component.html'
         })
-    ], AppComponent);
-    return AppComponent;
+    ], EmployeeComponent);
+    return EmployeeComponent;
 }());
-exports.AppComponent = AppComponent;
-//# sourceMappingURL=app.component.js.map
+exports.EmployeeComponent = EmployeeComponent;
+//# sourceMappingURL=employee.component.js.map
---
+++ Angular2Demo/src/app/Employee/employee.component.ts
@@ -0,0 +1,12 @@
+﻿import {Component} from '@angular/core'
+
+@Component({
+    selector: 'my-employee',
+    templateUrl: 'app/Employee/employee.component.html'
+})
+export class EmployeeComponent {
+    firstName: string = "Raghu"
+    lastName: string = "Rawat"
+    gender: string = "Male"
+    age: number = 27
+}
